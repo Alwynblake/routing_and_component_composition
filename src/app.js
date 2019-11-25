536873fae6b4c38b9518a4c67e3683bdc9418d00
Alistair Blake <consultations@me.com>
2019-11-24 21:24:16 -0800

    started

---
+++ src/app.js
@@ -0,0 +1,20 @@
+mport React from "react";
+
+// import Form from "./components/form";
+// import TypedRow from "./components/typedRow";
+
+import "./app.scss";
+
+// import "./styles.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nextId: 0,
+      thingsTyped: []
+    };
+  }
+
+
+export default App;
